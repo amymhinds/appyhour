fce0d5a16871d47765f1adf5a84768b971a47fc1
capetillo <carolinacapetilloc@gmail.com>
2020-01-08 18:49:46 -0800

    adds reviews and can create eater

--- main_app/models.py
+++ main_app/models.py
@@ -25,38 +25,33 @@ TASTE = (
     ('3', 'Both')
 )
 
-FOOD = (
-    ('A', 'American'),
-    ('B', 'Breakfast'),
-
-)
 # Create your models here.
 
 
 class Eater(models.Model):
     name = models.CharField(max_length=25)
     sex = models.CharField(
-        max_length=18,
+        max_length=17,
         choices=SEX,
         default=SEX[0][0]
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    interestedIn = models.CharField(
+    interested_in = models.CharField(
         max_length=18,
         choices=ORIENTATION,
-        default=ORIENTATION[0]
+        default=ORIENTATION[0][0]
     )
     age = models.PositiveIntegerField(
         validators=[MinValueValidator(18), MaxValueValidator(99)])
+    location = models.TextField(null=True, blank=True)
+    top_fav_cuisine = models.CharField(max_length=35, null=True, blank=True)
+    sec_fav_cuisine = models.CharField(max_length=35, null=True, blank=True)
+    third_fav_cuisine = models.CharField(max_length=35, null=True, blank=True)
+    least_fav_cuisine = models.CharField(max_length=35, null=True, blank=True)
 
 
-class FoodInterests(models.Model):
-    topFavCuisine = models.CharField(max_length=35, null=True, blank=True)
-    secFavCuisine = models.CharField(max_length=35, null=True, blank=True)
-    thirdFavCuisine = models.CharField(max_length=35, null=True, blank=True)
-    leastFavCuisine = models.CharField(max_length=35, null=True, blank=True)
-    taste = models.CharField(
-        max_length=6,
-        choices=TASTE,
-        default=TASTE[2]
-    )
+class Reviews(models.Model):
+    title = models.CharField(max_length=25, null=True, blank=True)
+    about_my_date = models.TextField(blank=True, null=True)
+    stars = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)], null=True)
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Eater, Reviews
 
-# Create your views here.
 
-# Add the following import
 from django.http import HttpResponse
 
 # Define the home view
@@ -10,3 +10,14 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Testing again /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+class EaterCreate(CreateView):
+    model = Eater
+    fields = ['name', 'sex', 'age', 'location', 'interested_in']
+
+
+class EaterUpdate(UpdateView):
+    model = Eater
+    fields = ['top_fav_cuisine', 'sec_fav_cuisine',
+              'third_fav_cuisine', 'least_fav_cuisine']
